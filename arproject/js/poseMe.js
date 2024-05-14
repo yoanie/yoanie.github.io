1d0f884b8961bd2296a2c1867d2b6892b9915048
dkrYoanie <raderk0777@gmail.com>
2024-05-13 21:50:33 -0700

    Add files via upload

--- arproject/js/poseMe.js
+++ arproject/js/poseMe.js
@@ -1,11 +1,11 @@
 AFRAME.registerComponent("pose-me", {
     init: function () {
         let el = this.el;
-
+    },
+    startGame: function () {
+        console.log("start game happened");
         let cam = document.querySelector("#cam");
-
         let scene = document.querySelector("a-scene");
-
         let score = document.querySelector("#score");
         let currentScore = 0;
 
@@ -16,12 +16,8 @@ AFRAME.registerComponent("pose-me", {
                 compareRotation(e.target);
             }
         });
-
-        //var raycasterEl = AFRAME.scenes[0].querySelector("[raycaster]");
-        //raycasterEl.components.raycaster.refreshObjects();
-
         //randomly set location
-        let numCircles = 2;
+        let numCircles = 1;
         for (let i = 0; i < numCircles; i++) {
             let bubble = document.createElement("a-entity");
             bubble.setAttribute("pose-me-target", true);
@@ -32,7 +28,7 @@ AFRAME.registerComponent("pose-me", {
         const compareRotation = function (otherCircle) {
             let marginOfError = 90;
             let camRotate = cam.getAttribute("rotation").z;
-            let otherRotate = otherCircle.getAttribute("rotation").z; 
+            let otherRotate = otherCircle.getAttribute("rotation").z;
 
             if (Math.abs(camRotate - otherRotate) <= marginOfError) {
                 //console.log("match!");
@@ -47,21 +43,11 @@ AFRAME.registerComponent("pose-me", {
             }
         };
     }
-    //tick: function(){
-    //    let cursor = document.createElement("a-plane");
-    //    let camera = document.querySelector("#cam");
-    //    //                        src="#bubble"
-    //                    //transparent="true"
-    //                    //material="shader: flat"
-    //                    //scale="0.5 0.5 1"
-    //                    //position="0 0 -.1"
-    //    cursor.setAttribute("src", "#bubble");
-    //    cursor.setAttribute("transparent", "true");
-    //    cursor.setAttribute("material", "shader: flat");
-    //    cursor.setAttribute("scale", "0.5 0.5 1");
-    //    a
-    //    cursor.setAttribute("render-order", "cursor");
-    //    
-    //    camera.appendChild(cursor);
+    //tick: function () {
+    //    let el = this.el;
+    //    let yPos = el.getAttribute("position").y;
+    //    if (yPos <= 0) {
+    //        this.el.updateAttribute();
+    //    }
     //}
 });
--- arproject/js/targetCircles.js
+++ arproject/js/targetCircles.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent("pose-me-target", {
         let bubble = this.el; //bubble positioning
         let flutter = document.createElement("a-entity"); //side-by-side animation
         let circle = document.createElement("a-plane"); //bubble image
-        let hitbox = document.createElement("a-circle");
+        let hitbox = document.createElement("a-plane");
         //let scene = document.querySelector("a-scene");
 
         const updateAttribute = function () {
@@ -13,11 +13,11 @@ AFRAME.registerComponent("pose-me-target", {
             let randomX = Math.random() * xVariation * 2 - xVariation;
             let randomZ = Math.random() * zVariation * 2 - zVariation - 5;
             let position = randomX + " .5 " + randomZ;
-            //let position = "0 .5 -5"; 
+            //let position = "0 .5 -5";
             bubble.setAttribute("position", position);
 
             //console.log("updated attribute");
-            
+
             //circle.setAttribute(
             //    "animation",
             //    `property: position;
@@ -34,17 +34,23 @@ AFRAME.registerComponent("pose-me-target", {
             );
             circle.setAttribute(
                 "animation",
-                "property: position;from:0 4 0; to: 0 0 0; loop:true; dur:" +((700*Math.random()) + 2500)+";direction:alternate; easing:linear; "
+                `property: position;
+                from:0 4 0; 
+                to: 0 0 0; 
+                loop:true; 
+                dur:${700 * Math.random() + 2500};
+                dir:alternate; 
+                easing:linear;`
             );
         };
-        
+
         const respawn = function () {
-            console.log("respawn");
+            //console.log("respawn");
             updateAttribute();
         };
-        
+
         this.el.updateAttribute = updateAttribute;
-        
+
         flutter.setAttribute(
             "animation",
             `property: position;
@@ -54,46 +60,47 @@ AFRAME.registerComponent("pose-me-target", {
             easing:easeInOutSine; 
             loop:true`
         );
-        //flutter.setAttribute("look-at", "true");
+        flutter.setAttribute("look-at", "true");
 
         circle.setAttribute("src", "#bubble");
-        
+
         //circle.setAttribute("class", "poseMeTarget");
         circle.setAttribute("material", "shader: flat");
         circle.setAttribute("transparent", "true");
-        circle.setAttribute("scale","2 2 1");
+        circle.setAttribute("scale", "2 2 1");
+        //circle.setAttribute("radius", 1);
         circle.setAttribute("look-at", "true");
         circle.setAttribute("position", "0 0 0.2");
         circle.setAttribute("render-order", "bubbles");
         //circle.setAttribute("opacity", "1");
 
-
         hitbox.setAttribute("class", "poseMeTarget");
         hitbox.setAttribute("material", "shader: flat; color: blue");
-        hitbox.setAttribute("opacity",".5");
+        hitbox.setAttribute("opacity", ".5");
         hitbox.setAttribute("position", "0 0 -0.1");
         hitbox.setAttribute("transparent", "true");
-        hitbox.setAttribute("scale",".25 .25 1");
+        hitbox.setAttribute("scale", ".25 .25 1");
         hitbox.setAttribute("render-order", "hitbox");
-        
+
         //circle.bind(updateAttribute);
         circle.setAttribute("reset", respawn);
-        
+
         updateAttribute();
-        
+
         //scene.appendChild(flutter);
         flutter.appendChild(circle);
         circle.appendChild(hitbox);
         bubble.appendChild(flutter);
-        
+
         //setInterval(updateAttribute, 3000);
     },
-    tick: function (){
-        let el = this.el;
-        let yPos = el.getAttribute("position").y;
-        if (yPos <= 0){
-            el.updateAttribute();
+    tick: function () {
+        if (initialize) {
+            let el = this.el;
+            let yPos = el.getAttribute("position").y;
+            if (yPos <= 0) {
+                el.updateAttribute();
+            }
         }
-
     }
 });
